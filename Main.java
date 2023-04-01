ffaa5efd58bbe88c0ac0cd0723c034fdcc7ddb69
diffloc <6632742+diffloc@users.noreply.github.com>
2023-04-01 13:08:02 -0500

    Stage 2/5 complete

--- Main.java
+++ Main.java
@@ -4,32 +4,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
         Scanner scanner = new Scanner(System.in);
         char[][] grid = new char[3][3];
         String userInput = scanner.nextLine();
-
         for (int i = 0; i < userInput.length(); i++) {
             grid[i / 3][i % 3] = userInput.charAt(i);
         }
-
-        for (int i = 0; i < grid.length; i++) {
-            if (i == 0) {
-                System.out.println("---------");
+        System.out.println("---------");
+        for (char[] row : grid) {
+            System.out.print("| ");
+            for (char c : row) {
+                System.out.print(c + " ");
             }
-            for (int j = 0; j < grid[i].length; j++) {
-                if (j == 0) {
-                    System.out.print("| ");
-                }
-                System.out.print(grid[i][j] + " ");
-                if (j == grid[i].length - 1) {
-                    System.out.print("|");
-                }
-            }
-            if (i == grid.length - 1) {
-                System.out.println("\n---------");
-            }
-            System.out.println();
+            System.out.println("|");
         }
+        System.out.println("---------");
     }
 }
